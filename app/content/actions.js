c9a41732389adc1fc6543421225085706b796b5c
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-08-18 21:52:41 -0400

    add example actions

---
+++ app/content/actions.js
@@ -0,0 +1,17 @@
+export function click() {
+    return {
+        type: 'CLICK'
+    };
+}
+
+export function addCount() {
+    return {
+        type: 'ADD_COUNT'
+    };
+}
+
+export function minusCount() {
+    return {
+        type: 'MINUS_COUNT'
+    };
+}
