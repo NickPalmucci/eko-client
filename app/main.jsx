2199996aab1ce7d286fccc2d4de42f7fcf382926
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-08-22 17:12:40 -0400

    add initializeCurrentLocation, will be worked into redux-saga later

--- app/main.jsx
+++ app/main.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import {render} from 'react-dom';
 import Root from './containers/root';
 import {clientStore} from './store';
+import {initializeCurrentLocation} from 'redux-little-router';
+
+const initialLocation = clientStore.getState().router;
+if (initialLocation) {
+    clientStore.dispatch(initializeCurrentLocation(initialLocation));
+}
 
 const el = document.getElementById('app');
 
