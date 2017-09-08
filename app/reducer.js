a2bd8a6737a8fbf34316d5499bb0159d9ceb17c1
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-09-08 01:43:51 -0400

    add saga and persist actions to reducer

--- app/reducer.js
+++ app/reducer.js
@@ -1,13 +1,27 @@
-import jobData from './sample-data.json';
+import {REHYDRATE} from 'redux-persist/constants';
 
 export default function exampleReducer(state, action) {
     if (state) {
         if (action.type === 'VIEW_JOB') {
-            const active = state.jobData.jobList[action.selected];
+            const active = state.ekoList[action.selected];
             return {
                 ...state,
                 selectedJob: active
             }
-        } else return state
-    } else return {jobData: jobData}
+        }
+        if (action.type === REHYDRATE) {
+            return {
+                ...state,
+                ...action.payload.appReducer
+            }
+        }
+        if (action.type === 'GET_JOBS_SUCCEEDED') {
+            return {
+                ...state,
+                ekoList: action.payload
+            }
+        }
+        return state
+    }
+    return null
 };
