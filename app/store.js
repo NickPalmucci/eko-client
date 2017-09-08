e84562b9dda8f7ec3ee4d3a0136448ae4e572360
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-09-08 01:42:31 -0400

    add redux-saga and redux-persist to store.js

--- app/store.js
+++ app/store.js
@@ -1,12 +1,25 @@
 import { combineReducers, compose, createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import {persistStore} from 'redux-persist';
 import { routerForBrowser } from 'redux-little-router';
+import rootSaga from './saga';
 import appReducer from './reducer';
-import {routes} from './routes'
+import {routes} from './routes';
+import jobList from './sample-data.json';
 
 const {reducer, middleware, enhancer} = routerForBrowser({routes});
 
+const sagaMiddleware = createSagaMiddleware();
+
 export const clientStore = createStore(
     combineReducers({ router: reducer, appReducer }),
-    {},
-    compose(enhancer, applyMiddleware(middleware))
+    {appReducer: {jobList: jobList}},
+    compose(enhancer,
+        applyMiddleware(middleware, sagaMiddleware),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
 );
+
+persistStore(clientStore);
+
+sagaMiddleware.run(rootSaga);
