e2a17810db670974e7d73fcec4538f0e93366dfb
Mucciano <nicolas.eli.palmucci@gmail.com>
2017-09-08 01:51:52 -0400

    call getJobs actions on root componentDidMount

--- app/containers/root.jsx
+++ app/containers/root.jsx
@@ -2,12 +2,17 @@ import React, {PropTypes} from 'react';
 import {Provider} from 'react-redux';
 import {Fragment} from 'redux-little-router';
 import AppLayout from './app-layout';
+import {getJobs} from '../modules/actions';
 
 
 export default class Root extends React.Component {
     static propTypes = {
         store: PropTypes.object.isRequired
     };
+
+    componentDidMount() {
+        this.props.store.dispatch(getJobs())
+    }
 
     render() {
         const {store} = this.props;
